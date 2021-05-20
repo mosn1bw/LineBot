19fa54048c8cc963521db9f3956b3c3806f08c5d
mosn1bw <81889769+mosn1bw@users.noreply.github.com>
2021-05-20 17:26:47 +0430

    Update main.go

--- main.go
+++ main.go
@@ -1,126 +1,118 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-const (
-	ActionEventOpen        = "open"
-	ActionEventClose       = "close"
-	ActionEventList        = "list"
-	ActionEventParticipate = "participate"
-	ActionEventLeave       = "leave"
-	ActionEventHelp        = "help"
-	ActionEventVote        = "vote"
-	ActionEventVoted       = "voted"
-	ActionEventStart       = "start"
-	ActionEventFinish      = "finish"
-	ActionEventCancel      = "cancel"
-)
+var postNumber = 4
 
-// TODO ファイルから読み出すように変更
-const HelpMessage = "このbotについて\nこのbotはLT会等で、参加者からアンケートを募集することを目的に作られています。\n\n以下のアクション一覧から利用したいコマンドを実行してください。"
+func messageReply(event linebot.Event) (err error) {
+	message := event.Message.(*linebot.TextMessage)
 
-type Line struct {
-	Bot *linebot.Client
+	if message.Text == "你在哪交換" {
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewLocationMessage("捷克理工大學", "Zikova 1903/4, 166 36 Praha 6", 50.102974, 14.391177)).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+	} else if message.Text == "給我看履歷" {
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("https://docs.google.com/document/d/1eKjTU4ebTTUJrISXR5YcQ47Jki-Br4apP_EQT_K_dbA/edit?usp=sharing")).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+	} else if message.Text == "你會哪些東西呢" {
+		skills := []string{"Golang", ".NET MVC", "Gitlab CI/CD", "Mysql", "Redis"}
+		replyMessage := "我會很多東西呢\n\n"
+
+		for _, skill := range skills {
+			replyMessage += "\n" + skill
+		}
+		replyMessage += "\n\n等等等..."
+
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+	} else if message.Text == "你有哪些興趣呢" {
+		skills := []string{"游泳", "打羽毛球", "看美劇", "踏青", "在床上耍廢"}
+		replyMessage := "我有很多興趣呢\n\n"
+
+		for _, skill := range skills {
+			replyMessage += "\n" + skill
+		}
+		replyMessage += "\n\n等等等..."
+
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+	} else if message.Text == "最近文章" {
+		template := crawlBlog(postNumber)
+		packMessage := linebot.NewTemplateMessage("哎呀~ 要用手機看歐", template)
+		if _, err := bot.ReplyMessage(event.ReplyToken, packMessage).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+	} else if message.Text == "嗨" {
+		packMessage := linebot.NewTemplateMessage("哎呀~ 要用手機看歐", questionTemplate)
+		if _, err := bot.ReplyMessage(event.ReplyToken, packMessage).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+
+	} else {
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("哎呀！你這樣問我有點困擾呢！")).Do(); err != nil {
+			log.Print(err)
+			return err
+		}
+	}
+
+	return nil
 }
 
-// New inject to domain services
-func NewLineBot(config *config.Line) *Line {
-	client, err := linebot.New(config.ChannelSecret, config.ChannelToken)
+func crawlBlog(num int) *linebot.CarouselTemplate {
+	template := linebot.NewCarouselTemplate()
+	carouselCols := []*linebot.CarouselColumn{}
+	// Request the HTML page.
+	res, err := http.Get("https://a28283878.github.io/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return &Line{client}
-}
-
-func (s *Server) callback(w http.ResponseWriter, r *http.Request) {
-	log.Println("callback")
-	ctx := r.Context()
-	reqests, err := s.Bot.ParseRequest(r)
-	for _, req := range reqests {
-		fmt.Printf("%#v\n", req)
-		var response linebot.SendingMessage
-		switch req.Type {
-		case linebot.EventTypeMessage:
-			switch message := req.Message.(type) {
-			case *linebot.TextMessage:
-				switch message.Text {
-				// リッチメニューボタン
-				case ActionEventOpen:
-					response = s.getMessageOpenEvent(ctx, req)
-				case ActionEventClose:
-					response = s.getMessageCloseEvent(ctx, req)
-				case ActionEventList:
-					response = s.getMessageEvents(ctx, req)
-				case ActionEventVote:
-					response = s.getMessageVoteList(ctx, req)
-				case ActionEventLeave:
-					response = s.getMessageLeaveEvent(ctx, req)
-				case ActionEventHelp:
-					response = linebot.NewTextMessage(HelpMessage)
-				// 確認処理ボタン
-				case ActionEventStart:
-					response = s.getMessageStartEvent(ctx, req)
-				case ActionEventFinish:
-					response = s.getMessageFinishEvent(ctx, req)
-				case ActionEventCancel:
-					response = linebot.NewTextMessage("処理を中断しました")
-				default:
-					if strings.Contains(message.Text, ActionEventParticipate) {
-						splits := strings.Split(message.Text, " ")
-						log.Println(message.Text)
-						eventID := domain.EventID(splits[1])
-						response = s.getMessageParticipateEvent(ctx, req, eventID)
-					} else if strings.Contains(message.Text, ActionEventVoted) {
-						splits := strings.Split(message.Text, " ")
-						response = s.getMessageVoteEvent(ctx, req, splits[1])
-					} else {
-						response = linebot.NewTextMessage(message.Text)
-					}
-				}
-			}
-		case linebot.EventTypeFollow:
-			response = s.getMessageFollowAction(ctx, req)
+	// Find the review items
+	doc.Find(".posts-wrapper article").Each(func(i int, s *goquery.Selection) {
+		if i >= num {
+			return
 		}
+		title := s.Find("a").Text()
+		postURL, _ := s.Find("a").Attr("href")
+		pictureURL, _ := s.Find("a").Find("div").Attr("style")
+		pictureURL = pictureURL[strings.Index(pictureURL, "(")+1 : strings.Index(pictureURL, ")")]
 
-		// 全処理をここで一括
-		if _, err = s.Bot.ReplyMessage(req.ReplyToken, response).Do(); err != nil {
-			fmt.Println(err)
-		}
-	}
+		btn := linebot.NewURITemplateAction("觀看", postURL)
+		column := linebot.NewCarouselColumn(pictureURL, "文章", title, btn)
+
+		carouselCols = append(carouselCols, column)
+	})
+
+	template = linebot.NewCarouselTemplate(carouselCols...)
+
+	return template
 }
-
-var bot *linebot.Client
-
-func main() {
-	var err error
-	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
-	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", callbackHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
-}
-
 func (bot *OperatorImpl) ReactToPostback(event *linebot.Event) error {
 	var (
 		data         = strings.Split(event.Postback.Data, ":")
